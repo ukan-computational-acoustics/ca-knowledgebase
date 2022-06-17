7b9a2bf1051ac4b94893607833fe88b81fbde08a
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-17 10:41:01 +0100

    try removing config (I think it's not needed)

--- tutorials/fem/fenicsx-helmholtz-tutorial.py
+++ tutorials/fem/fenicsx-helmholtz-tutorial.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 # # The Helmholtz Equation with FEniCSx
 # Authors: Stefano Tronci and Jørgen S. Dokken
 #
