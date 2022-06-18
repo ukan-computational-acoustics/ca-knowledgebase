3eee9ffaef24b7f2339fe9b2afe28dbf751409e6
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-18 07:32:46 +0100

    move back to using notebook files

--- _test/test_tutorials.py
+++ _test/test_tutorials.py
@@ -1,11 +1,13 @@
 import os
 import pytest
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 
 def find_python_files(dir, subdir=None):
     out = []
     for i in os.listdir(dir):
-        if i.startswith("."):
+        if i.startswith(".") or i.startswith("_"):
             continue
         new_dir = os.path.join(dir, i)
         if subdir is None:
@@ -14,17 +16,22 @@ def find_python_files(dir, subdir=None):
             new_subdir = os.path.join(subdir, i)
         if os.path.isdir(new_dir):
             out += find_python_files(new_dir, new_subdir)
-        elif i.endswith(".py"):
+        elif i.endswith(".ipynb"):
             out.append(new_subdir)
     return out
 
 
 tutorial_dir = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
-    os.path.join("..", "tutorials"))
+    "..")
 tutorials = find_python_files(tutorial_dir)
 
 
-@pytest.mark.parametrize("tutorial", tutorials)
-def test_tutorial(tutorial):
-    assert os.system(f"python3 {os.path.join(tutorial_dir, tutorial)}") == 0
+@pytest.mark.parametrize("notebook", tutorials)
+def test_tutorial(notebook):
+    with open(os.path.join(tutorial_dir, notebook)) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600)
+
+    ep.preprocess(nb, {"metadata": {"path": path}})
