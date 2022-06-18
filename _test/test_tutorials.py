241cdd1ebea234d83c39d741a7b54d059c3d8d30
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-18 07:36:01 +0100

    flake

--- _test/test_tutorials.py
+++ _test/test_tutorials.py
@@ -4,32 +4,27 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
-def find_python_files(dir, subdir=None):
+def find_notebooks(dir):
     out = []
     for i in os.listdir(dir):
         if i.startswith(".") or i.startswith("_"):
             continue
-        new_dir = os.path.join(dir, i)
-        if subdir is None:
-            new_subdir = i
-        else:
-            new_subdir = os.path.join(subdir, i)
-        if os.path.isdir(new_dir):
-            out += find_python_files(new_dir, new_subdir)
+        if os.path.isdir(os.path.join(dir, i)):
+            out += find_notebooks(os.path.join(dir, i))
         elif i.endswith(".ipynb"):
-            out.append(new_subdir)
+            out.append((dir, i))
     return out
 
 
 tutorial_dir = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "..")
-tutorials = find_python_files(tutorial_dir)
+tutorials = find_notebooks(tutorial_dir)
 
 
-@pytest.mark.parametrize("notebook", tutorials)
-def test_tutorial(notebook):
-    with open(os.path.join(tutorial_dir, notebook)) as f:
+@pytest.mark.parametrize("path, notebook", tutorials)
+def test_tutorial(path, notebook):
+    with open(os.path.join(path, notebook)) as f:
         nb = nbformat.read(f, as_version=4)
 
     ep = ExecutePreprocessor(timeout=600)
